a70eafcc9a1852aac2cfe88549d5234eaf1d473a
ocefpaf <ocefpaf@gmail.com>
2013-08-15 19:58:12 -0300

    Better and improved `compile.sh`.

--- compile.sh
+++ compile.sh
@@ -12,27 +12,36 @@
 # obs:
 #
 
+LATEXMK=/usr/bin/latexmk
+PANDOC=/home/filipe/bin/pandoc
+
 DIR=$1
 slides=$(mktemp --dry-run)  # Slides.
 handouts=$(mktemp --dry-run)  # Handouts.
 
-cat slides.tex header.tex ${DIR}/lecture.tex > $slides
-cat handouts.tex header.tex ${DIR}/lecture.tex > $handouts
+datestring=$(date +"%d_%b_%Y")
 
-OPTS="-pdf -latexoption=-interaction=batchmode"
+cat common/slides.tex common/header.tex ${DIR}/lecture.tex > $slides
+cat common/handouts.tex common/header.tex ${DIR}/lecture.tex > $handouts
+
+OPTION="--mathjax --smart --normalize --standalone \
+        --highlight-style=pygments --webtex"
+FROM="--from markdown homework.md"
+DOCX="--to html --output ${datestring}_OM_homework.html"
+HTML="--to docx --output ${datestring}_OM_homework.docx"
+LATEX="--to latex --output ${datestring}_OM_homework.pdf"
+
 pushd ${DIR}
-    \latexmk $OPTS --jobname=slides $slides
-    \latexmk $OPTS --jobname=handouts $handouts
+    OPTS="-pdf -latexoption=-interaction=batchmode"
+    # Slides.
+    $LATEXMK $OPTS --jobname=${datestring}_OM_slides $slides
+
+    # Handouts.
+    $LATEXMK $OPTS --jobname=${datestring}_OM_handouts $handouts
+
+    # Homework.
+    $PANDOC $OPTION $FROM $LATEX
+
+    # Clean-up.
     rm *.snm *.nav *.fdb_latexmk *.fls *.log *.out *.toc *.aux
 popd
-
-# Homework.
-OPTION="--mathjax --smart --normalize --standalone --highlight-style=pygments --webtex"
-FROM="--from markdown ${DIR}/homework.md"
-DOCX="--to html --output ${DIR}/homework.html"
-HTML="--to docx --output ${DIR}/homework.docx"
-LATEX="--to latex --output ${DIR}/homework.pdf"
-
-#pandoc $OPTION $FROM $HTML
-#pandoc $OPTION $FROM $DOCX
-pandoc $OPTION $FROM $LATEX
